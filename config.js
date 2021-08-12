e7c7d5ad8ee3296917e1f55a694f09e285fbe5bd
sonll1 <88782564+sonll1@users.noreply.github.com>
2021-08-12 23:16:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hey Girl!',
+    introDesc: `Nhân dịp sinh nhật lần thứ 20
+    của em,anh chúc anh luôn tươi khỏe, trẻ đẹp
+    Cầu mong những gì may mắn nhất
+    tốt đẹp nhất và hạnh phúc nhất sẽ đến với em trong tuổi mới`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Chúc Mừng Sinh Nhật Em.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com'https://www.facebook.com/messages/t/100054044436409/
 }
